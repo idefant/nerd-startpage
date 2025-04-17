aeeb5d560aa7c7e8d6374b68880f3ea00988c4dc
idefant <inik3004@gmail.com>
2025-04-17 09:00:11 +0500

    fix bug with getting text from clipboard

--- src/utils/getTextFromClipboard.ts
+++ src/utils/getTextFromClipboard.ts
@@ -1,8 +1,9 @@
 import { toast } from 'react-toastify';
 
-export const getTextFromClipboard = () => {
+export const getTextFromClipboard = async () => {
   try {
-    return navigator.clipboard.readText();
+    const text = await navigator.clipboard.readText();
+    return text;
   } catch {
     toast.error('Не удалось получить доступ к буферу обмена');
     return undefined;
